2cea7d74aa811737b6508f894a5ea544a40fadff
aryafadhilaazaya04 <aryaazaya00@gmail.com>
2025-06-20 17:05:51 +0700

    Layouting system

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Arya Blog | Home</title>
-</head>
-<body>
-    <h1>Welcome to Arya Blog</h1>
+@extends('layouts.main')
+@section('content')
 
-    <script src="js/script.js"></script>
-</body>
-</html>
+<h1>Halaman Home</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Arya Fadhila Azaya - MI.22.R1",
         "email" => "aryafadhila@gmail.com",
         "image" => "AvatarAI.jpg"
@@ -15,5 +18,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
