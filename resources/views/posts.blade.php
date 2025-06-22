821503a77854bdf3c661490d04c522d5590a8c11
aryafadhilaazaya04 <aryaazaya00@gmail.com>
2025-06-22 21:35:42 +0700

    Memindahkan eager loading ke model, membuat card hero posts, menambahkan image

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,26 @@
 @section('content')
 
 <h1 class="mb-4">{{ $title }}</h1>
+
+@if ($posts->count())
+    <div class="card mb-3">
+        <img src="https://picsum.photos/seed/1070/720" class="card-img-top" alt="{{ $posts[0]->category->name }}" style="width: 100%; height: 150px; object-fit: cover; margin: 0 auto; display: block;">
+        <div class="card-body text-center">
+            <h3 class="card-title">{{ $posts[0]->title }}</h3>
+            <p>
+                <small class="text-muted">
+                    By: <a href="/authors/{{ $posts[0]->user->username }}" class="text-decoration-none">{{ $posts[0]->user->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                    {{ $posts[0]->created_at->diffForHumans() }}
+                </small>
+            </p>
+            <p class="card-text">{{ $posts[0]->excerpt }}</p>
+            <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-outline-primary">Read more...</a>
+        </div>
+    </div>
+@else
+    <p class="text-center fs-4">No post found.</p>
+@endif
+
 @foreach ($posts as $post)
 <article class="mb-3 border-bottom pb-3">
     <h2>
