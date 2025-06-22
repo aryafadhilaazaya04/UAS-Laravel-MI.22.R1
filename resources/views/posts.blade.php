57131ecf2f6dbd0fb72378408933297ab7567793
aryafadhilaazaya04 <aryaazaya00@gmail.com>
2025-06-22 12:00:07 +0700

    Menambahkan link author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,12 +3,13 @@
 
 <h1 class="mb-4">Halaman Blog Posts</h1>
 @foreach ($posts as $post)
-<article>
+<article class="mb-3 border-bottom pb-3">
     <h2>
-        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
     </h2>
-    <h5>By: {{ $post->author }}</h5>
+    <p>By: {{ $post->author }} in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
     <p>{{ $post->excerpt }}</p>
+    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
 </article>
 @endforeach
 
