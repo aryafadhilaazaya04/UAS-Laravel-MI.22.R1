b911428ee494ad4b9e50129b4c0df6220ef37176
aryafadhilaazaya04 <aryaazaya00@gmail.com>
2025-06-22 13:02:19 +0700

    Membuat halaman authors

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,10 +3,10 @@
 
 <article class="mb-5">
     <h2>{{ $post->title }}</h2>
-    <p>By: {{ $post->author }} in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 </article>
 
-<a href="/posts">Back to Posts</a>
+<a href="/posts" class="d-block">Back to Posts</a>
 
 @endsection
