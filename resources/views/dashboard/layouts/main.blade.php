dccddc79c146d9dfc99c50f9a49b4b3c462b5d72
aryafadhilaazaya04 <aryaazaya00@gmail.com>
2025-06-24 15:48:41 +0700

    Mengubah view menjadi layout

---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Arya Blog | @yield('title', 'Dashboard')</title>
+    <!-- Bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+    <!-- Custom style -->
+    <link href="/css/dashboard.css" rel="stylesheet" />
+    @stack('styles')
+</head>
+
+<body>
+    @include('dashboard.layouts.header')
+    <div class="container-fluid">
+        <div class="row">
+            @include('dashboard.layouts.sidebar')
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4 bg-light min-vh-100">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+    <script src="/js/dashboard.js"></script>
+    @stack('scripts')
+</body>
+
+</html>
