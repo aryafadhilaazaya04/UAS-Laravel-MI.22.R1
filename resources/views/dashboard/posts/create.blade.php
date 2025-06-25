e8e6c97b7ddea4c539a391270d15416badb695b0
aryafadhilaazaya04 <aryaazaya00@gmail.com>
2025-06-25 07:41:38 +0700

    Membuat view create post

---
+++ resources/views/dashboard/posts/create.blade.php
@@ -0,0 +1,22 @@
+@extends('dashboard.layouts.main')
+@section('content')
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+    <h1 class="h2">Create New Post</h1>
+</div>
+<div class="col-lg-8">
+    <form method="POST" action="/dashboard/posts">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" name="title" required autofocus>
+        </div>
+        <div class="mb-3">
+            <label for="slug" class="form-label">Slug</label>
+            <input type="text" class="form-control" id="slug" name="slug" required autofocus>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">Create Post</button>
+    </form>
+</div>
+
+@endsection
